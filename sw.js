37392df538bbdf67bdc12a13bbb96a69a7141b5b
Rozi Khan <khanrules@Rozis-MacBook-Pro.local>
2026-09-20 15:12:33 +0400

    Align storefront release module graph

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "plugngo-store-v3-b79ae23";
+const CACHE = "plugngo-store-v4-4849798";
 const SHELL = ["/", "/index.html", "/manifest.webmanifest", "/plugngo-icon-192.png", "/plugngo-icon-512.png"];
 
 self.addEventListener("install", (event) => {
