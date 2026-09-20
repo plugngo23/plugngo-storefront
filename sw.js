2b4aa9b997b7fddb946e0a85cf064c41efa0960d
Rozi Khan <khanrules@Mac.lan>
2026-09-20 22:30:08 +0400

    Publish customer catalogue quality cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "plugngo-store-v6-0b659cd";
+const CACHE = "plugngo-store-v7-d0efe83";
 const SHELL = ["/", "/index.html", "/manifest.webmanifest", "/plugngo-icon-192.png", "/plugngo-icon-512.png"];
 
 self.addEventListener("install", (event) => {
