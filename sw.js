a91867bceea29fea4618a9f6d0d192f1711fd2d2
Rozi Khan <khanrules@Rozis-MacBook-Pro.local>
2026-09-23 03:46:36 +0400

    Explain prelaunch requests and keep checkout off

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "plugngo-store-v8-6abb0e3";
+const CACHE = "plugngo-store-v9-20260923";
 const SHELL = ["/", "/index.html", "/manifest.webmanifest", "/plugngo-icon-192.png", "/plugngo-icon-512.png"];
 
 self.addEventListener("install", (event) => {
